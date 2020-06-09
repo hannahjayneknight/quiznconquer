f6a90a64b49a8eff3d8b56bb4a65bc477db81b95
hannahjayneknight <miss.knight19@imperial.ac.uk>
2020-06-09 22:15:50 +0100

    08/06/20

--- app/server.js
+++ app/server.js
@@ -38,53 +38,55 @@ let numWS = [];
 app.ws("/", function (ws, req) {
 
     // THE TIMER
-    function startTimer() {
+    function startTimer(wsarr) {
         const mins = 0.5;
         const now = Date.now();
         const deadline = mins * 60 * 1000 + now;
 
-        var timerID = setInterval(function () {
-            var currentTime = Date.now();
-            var distance = deadline - currentTime;
-            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        let timerID = setInterval(function () {
+            let currentTime = Date.now();
+            let distance = deadline - currentTime;
+            let seconds = Math.floor((distance % (1000 * 60)) / 1000);
             if (parseInt(seconds) === 0) {
                 clearInterval(timerID);
-                ws.send(JSON.stringify({
+                wsarr.forEach((thisws) => thisws.send(JSON.stringify({
                     "timer": seconds,
                     "gameStatus": "gameOver"
-                }));
+                })));
             } else {
-                ws.send(JSON.stringify({
+                wsarr.forEach((thisws) => thisws.send(JSON.stringify({
                     "timer": seconds,
                     "gameStatus": "playing"
-                }));
+                })));
             }
         // rather than change html, the server will send
         // JSON.stringify({"timer": seconds})
         }, 500);
-        ws.send(JSON.stringify({
+        wsarr.forEach((thisws) => thisws.send(JSON.stringify({
             "timer": 30,
             "gameStatus": "playing"
-        }));
+        })));
     }
 
     // This ensure that it waits for 4 players to join
-    let playerNum = numWS.length;
     // eg player1 = 0 etc
     // this is an object
-    ws.myprivatedata = { "playerNumber": numWS.length, "players": numWS };
+    ws.myprivatedata = {"playerNumber": numWS.length, "players": numWS};
     // numWS contains all the ws socket objects
-    numWS.push( ws );
-    if ( numWS.length === 4 ) {
+    numWS.push(ws);
+    if (numWS.length === 4) {
         numWS = [];
-        ws.myprivatedata.players.forEach( () => startTimer());
-    }
+        startTimer(ws.myprivatedata.players);
 
-    // generates a testing word and sends it to the client to be
-    // displayed on the DOM
-    let word = generateWord(dictionary.beginner);
-    ws.myprivatedata.players.forEach( () =>
-        ws.send(JSON.stringify( { "word": word[0] } ) ));
+        // generates a testing word and sends it to the client to be
+        // displayed on the DOM
+        ws.myprivatedata.players.forEach(function (thisws) {
+            thisws.myprivatedata.word = generateWord(dictionary.beginner);
+            thisws.send(JSON.stringify({
+                "word": thisws.myprivatedata.word[0]
+            }));
+        });
+    }
 
     // when receiving a message from the client...
     ws.on("message", function (msg) {
@@ -96,21 +98,21 @@ app.ws("/", function (ws, req) {
         if (clientObj.answer !== undefined) {
             // it will check if it is correct
             // and send the player number that won
-            if (clientObj.answer === word[1]) {
+            if (clientObj.answer === ws.myprivatedata.word[1]) {
                 ws.send(JSON.stringify({
                     "playerWon": 1
                 }));
             }
             // it will generate a new word for questioning
             // ONLY for the player that won the tile
-            word = generateWord(dictionary.beginner);
-            ws.send(JSON.stringify( { "word": word[0] } ) );
+            ws.myprivatedata.word = generateWord(dictionary.beginner);
+            ws.send(JSON.stringify({"word": ws.myprivatedata.word[0]}));
 
 
         // if the message is the tile number to be won, it will send
         // the tile number to be changed and the player that won it
         } else if (clientObj.winningTile !== undefined) {
-            var tileStolen = {
+            let tileStolen = {
                 "tileStolen": {"winner": 1, "tileNumber": clientObj.winningTile}
             };
             ws.send(JSON.stringify(tileStolen));
