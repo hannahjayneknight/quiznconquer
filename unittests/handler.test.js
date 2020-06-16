0fdaab7cf14f848d09b6576e20eb0207762c8738
hannahjayneknight <miss.knight19@imperial.ac.uk>
2020-06-16 12:25:30 +0100

    16/06/20

--- unittests/handler.test.js
+++ unittests/handler.test.js
@@ -10,15 +10,7 @@ const it = window.it;
 describe("Example Based Testing", function () {
     // "it" is the function that describes an individual test
     it("getIndexOfK function works as expected", function () {
-        const divNums = [
-            [0, 1, 2, 3, 4, 5],
-            [6, 7, 8, 9, 10, 11],
-            [12, 13, 14, 15, 16, 17],
-            [18, 19, 20, 21, 22, 23],
-            [24, 25, 26, 27, 28, 29],
-            [30, 31, 32, 33, 34, 35]
-        ];
-        const index = H.getIndexOfK(divNums, 5);
+        const index = H.getIndexOfK(5);
         const expected = [5, 0];
         if (!T.oneDArrEquals(index, expected)) {
             throw "getIndexOfK did not work as expected";
@@ -26,3 +18,6 @@ describe("Example Based Testing", function () {
 
     });
 });
+
+
+// do a test to check that server currentBoard is as expected?
