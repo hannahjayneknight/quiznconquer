5a7008385cce07ef66972d59796913a0d6f18deb
hannahjayneknight <miss.knight19@imperial.ac.uk>
2020-07-27 15:35:35 +0100

    27/07/20

---
+++ app/games.js
@@ -0,0 +1,22 @@
+/*
+
+1. create an object with all the games in.
+
+One game would look like this:
+
+games.WXYZ = {
+
+    players: {
+        player1: { player web socket object goes here },
+        player2: { player web socket object goes here },
+        player3: { player web socket object goes here },
+        player4: { player web socket object goes here }
+    },
+    public: true
+
+}
+
+This means it can give information on the players and
+whether it is private or public.
+
+*/
